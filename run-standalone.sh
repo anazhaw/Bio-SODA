08deb8a0d27745d857f365adbe1e17a2085479f9
Sima Ana-Claudia (simn) <simn@zhaw.ch>
2020-12-01 14:50:25 +0100

    run on port 8091

--- run-standalone.sh
+++ run-standalone.sh
@@ -13,7 +13,8 @@ $JAVAHOME/bin/java -version
 #
 # Run standalone
 #
-$JAVAHOME/bin/java -cp $CLASSPATH -XX:ParallelGCThreads=4 -Xmx46G ch.ethz.semdwhsearch.prototyp1.RunStandaloneSemDwhSearch -p 8081
+$JAVAHOME/bin/java -cp $CLASSPATH -XX:ParallelGCThreads=4 -Xmx46G ch.ethz.semdwhsearch.prototyp1.RunStandaloneSemDwhSearch -p 8091
+
 
 
 
